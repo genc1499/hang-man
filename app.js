26fe0c617a156af294bb8fb7da33af1f44e20bb5
Joe Gencarelli <jp.genc@gmail.com>
2022-08-24 16:56:55 -0400

    Add functionality for guessing a letter and checking each guess

--- app.js
+++ app.js
@@ -1,1 +1,68 @@
-console.log("hi")
+// app object
+
+const app={};
+
+app.init = ()=>{
+    app.getGuess();
+    app.count = 5;
+    app.word="hello";
+    app.array=["_","_","_","_","_"];
+    app.guessed=[];
+  
+}
+
+// check guess
+app.checkGuess = (word) =>{
+    // check if user has already entered a letter
+    let checked = app.guessed.filter(item=>{
+        return item===word
+    })
+
+    if(checked.length>0)
+    {
+        alert(`you already guessed letter ${word} `);
+    }
+
+
+    else{
+
+    let newS = app.word.split("")
+    console.log(newS);
+    newS.forEach((item,index) =>{
+        if(item===word){
+            app.array[index]=item
+            console.log(app.array)
+            app.guessed.push(word);
+
+            
+        }
+    
+    })
+
+}
+
+   
+    
+    
+
+}
+// get user's guess
+
+app.getGuess = () =>{
+
+
+let guess = document.querySelector("form");
+guess.addEventListener('submit', function(e){
+    e.preventDefault();
+    let userGuess= e.target.childNodes[1].value;
+    app.checkGuess(userGuess);
+})
+
+
+
+}
+
+
+app.init();
+
+
