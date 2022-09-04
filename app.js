a9ea5eaf781eeae24973868b18345f811edbb570
Joe Gencarelli <jp.genc@gmail.com>
2022-09-04 13:22:46 -0400

    Added count functionality to check the number of user guesses

--- app.js
+++ app.js
@@ -2,20 +2,23 @@
 
 const app={};
 
+
 app.init = ()=>{
     app.getGuess();
-    app.count =5;
     app.word="hello";
     app.array=["_","_","_","_","_"];
     app.guessed=[];
     app.wordGuess();
+    app.count=0;
   
 }
-// Check word guess
 
+
+
+// Check word guess
 app.checkWordGuess=(word)=>{
     if(word===app.word){
-        alert("Correct!")
+        alert("You guessed the word!")
     }
 }
 
@@ -28,15 +31,15 @@ app.wordGuess = () =>{
 }
 
 // check guess
-app.checkGuess = (word) =>{
+app.checkGuess = (letter) =>{
     // check if user has already entered a letter
     let checked = app.guessed.filter(item=>{
-        return item===word
+        return item===letter
     })
 
     if(checked.length>0)
     {
-        alert(`you already guessed letter ${word} `);
+        alert(`you already guessed letter ${letter} `);
     }
 
 
@@ -45,12 +48,10 @@ app.checkGuess = (word) =>{
     let newS = app.word.split("")
     console.log(newS);
     newS.forEach((item,index) =>{
-        if(item===word){
+        if(item===letter){
             app.array[index]=item
             console.log(app.array)
-            app.guessed.push(word);
-
-            
+            app.guessed.push(letter);   
         }
     
     })
@@ -70,16 +71,24 @@ app.getGuess = () =>{
 let guess = document.getElementById("guessLetter");
 guess.addEventListener('submit', function(e){
     e.preventDefault();
-    app.count-=1;
+    app.count+=1;
+    if(app.count>5){
+        alert(`No more Guess, the word was ${app.word}`);
+        
+    }
+    else{
+    console.log(app.count);
     let userGuess= e.target.childNodes[1].value;
     guess.reset();
     app.checkGuess(userGuess);
- 
+    }
 })
 
 
 
 }
+
+
 
 
 
