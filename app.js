47e4bc1c544c7b7268380cf792962edc4e302f10
Joe Gencarelli <jp.genc@gmail.com>
2022-09-05 03:06:06 -0400

    Added turns left and refactored API call

--- app.js
+++ app.js
@@ -9,12 +9,21 @@ app.init = ()=>{
     app.array=[];
     app.guessed=[];
     app.wordGuess();
-    app.count=0;
+    app.count=6;
     app.wordMatchCount=0;
+    app.displayGuessesLeft();
+    app.countCheck = 0;
+    app.letterCount=0;
 }
 
+app.displayGuessesLeft=()=>{
+    const turnsLeft=document.getElementById('turns-left');
+    turnsLeft.innerText=String(app.count);
+}
+
+
 app.getRandomWord=()=>{
-    fetch('https://random-word-api.herokuapp.com/word?length=6').then((response)=>response.json()).then((data)=>{console.log(data);
+    fetch('https://random-word-api.herokuapp.com/word?length=5').then((response)=>response.json()).then((data)=>{console.log(data);
 
     app.word=data[0];
     app.array= app.word.split("").map(item =>{return ("_")});
@@ -22,6 +31,8 @@ app.getRandomWord=()=>{
 
     })
 }
+
+
 
 // Display word so far...
 app.displayWord=(array)=>{
@@ -33,6 +44,10 @@ app.displayWord=(array)=>{
 app.checkWordGuess=(word)=>{
     if(word===app.word){
         alert("You guessed the word!")
+    }
+    else{
+        alert("Sorry, that's not the word!");
+        count.app-=1;
     }
 }
 
@@ -59,23 +74,37 @@ app.checkGuess = (letter) =>{
 
     else{
 
-    let wordArray = app.word.split("")
-    wordArray.forEach((item,index) =>{
-        if(item===letter){
-            app.array[index]=item
-            console.log(app.array)
-            app.guessed.push(letter);  
-            let hang = document.getElementById('hang-area');
-            hang.innerText=app.array.join(" ");
-        }
+        let wordArray = app.word.split("")
+        wordArray.forEach((item,index) =>{
+            if(item===letter){
+                app.array[index]=item
+                console.log(app.array)
+                app.guessed.push(letter);  
+                let hang = document.getElementById('hang-area');
+                hang.innerText=app.array.join(" ");
+                console.log('corrrect', app.count);
 
-        if(app.array.join("")===app.word){
-            alert("You guessed the word!")
-        }
-    
-    })
+                // Keep track of letters that match 
+                const regex = /[a-zA-Z]/g; 
+                app.letterCount=app.array.join(" ").match(regex).length
+                
+            }
+        })
 
-}
+        // If a letter is not found in array, letter guess doesnt match, remove a guess from count
+        if(app.letterCount===app.countCheck){
+            app.count-=1;
+            app.countCheck=app.letterCount;
+            console.log(app.count);
+            app.displayGuessesLeft();
+        }
+         
+        // Make into a function
+            if(app.array.join("")===app.word){
+                alert("You guessed the word!")
+            }
+
+    }
 
 
 }
@@ -88,8 +117,8 @@ app.getGuess = () =>{
         e.preventDefault();
 
         // increase guess count and check against the number of guesses left
-        app.count+=1;
-        if(app.count>5){
+       
+        if(app.count===0){
             alert(`No more Guess, the word was ${app.word}`);   
         }
 
@@ -98,7 +127,7 @@ app.getGuess = () =>{
         let userGuess= e.target.childNodes[1].value;
         guess.reset();
         const guessList=document.getElementById('guesses');
-
+        // app.count+=1;
         // Display guessed letters
         guessList.append(userGuess+" ")
         app.checkGuess(userGuess.toLowerCase());
