385d5865626845ca283c4d5daca144245a5e0d10
Joe Gencarelli <jp.genc@gmail.com>
2022-09-06 20:23:40 -0400

    Added CSS/JS to draw 'hang man'when user guesses incorrectly

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.init = ()=>{
     app.count=6;
     app.displayGuessesLeft(); 
     app.reg = /[a-zA-Z]/ ;
+    app.drawCount = 0;
+    app.hangMan = ['head','torso','leftleg','rightleg','leftarm','rightarm'];
 }
 
 app.displayGuessesLeft=()=>{
@@ -21,11 +23,14 @@ app.displayGuessesLeft=()=>{
 
 // API Call to get new word
 app.getRandomWord=()=>{
-    fetch('https://random-word-api.herokuapp.com/word?length=5').then((response)=>response.json()).then((data)=>{
+    let randNum = Math.floor(Math.random() * (6 - 4 + 1) + 4);
+    fetch(`https://random-word-api.herokuapp.com/word?length=${randNum}`).then((response)=>response.json()).then((data)=>{
 
     app.word=data[0];
     app.array= app.word.split("").map(item =>{return ("_")});
+    console.log(app.word);
     app.displayWord(app.array)
+
 
     })
 }
@@ -58,7 +63,7 @@ app.wordGuess = () =>{
 // Check the letter the user has entered
 app.checkGuess = (letter) =>{
 
-    // Construct new regex object for each lette guessed
+    // Construct new regex object for each letter guessed
 
     let re = new RegExp(letter)
 
@@ -67,14 +72,25 @@ app.checkGuess = (letter) =>{
         return item===letter
     })
 
-    if(checked.length>0)
+    app.guessed.push(letter);
+
+    if(checked.length>0 && app.guessed.length>1)
     {
+        app.guessed.splice(0,app.guessed.length-1);
         alert(`you already guessed letter ${letter} `);
+        
     }
 
     // If test method is false, remove a turn and display the number of turns left
     if(!re.test(app.word)){
         app.count-=1;
+        let hanger = document.querySelector('.wrapper')
+        let draw = document.createElement('div');
+        
+        draw.classList.add(`${app.hangMan[app.drawCount]}`);
+        hanger.append(draw);
+        app.drawCount+=1;
+
         app.displayGuessesLeft();
     }
 
@@ -84,8 +100,7 @@ app.checkGuess = (letter) =>{
         let wordArray = app.word.split("")
         wordArray.forEach((item,index) =>{
             if(item===letter){
-                app.array[index]=item
-                app.guessed.push(letter);  
+                app.array[index]=item 
                 let hang = document.getElementById('hang-area');
                 hang.innerText=app.array.join(" ");  
             }
@@ -107,22 +122,26 @@ app.getGuess = () =>{
     let guess = document.getElementById("guessLetter");
     guess.addEventListener('submit', function(e){
         e.preventDefault();
-
+        let userGuess= e.target.childNodes[1].value;
+        let re = new RegExp(userGuess)
         // increase guess count and check against the number of guesses left
        
-        if(app.count===0){
+        if(app.count===0 && !re.test(app.word)){
+          
             alert(`No more Guess, the word was ${app.word}`);   
         }
 
         else{
        
-            let userGuess= e.target.childNodes[1].value;
+           
             // Check if user input is a letter
+          
             if(app.reg.test(userGuess)){
                 guess.reset();
                 const guessList=document.getElementById('guesses');
                 // app.count+=1;
                 // Display guessed letters
+                
                 guessList.append(userGuess+" ")
                 app.checkGuess(userGuess.toLowerCase());
             }
